094d8a8b1c8b4e8b6eed5f740faa5bcd79987dfd
Devgupta1432 <100144102+Devgupta1432@users.noreply.github.com>
2024-10-17 21:21:30 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,17 +7,22 @@ import Navbar from "./components/navbar";
 import "./css/card.scss";
 import "./css/globals.scss";
 import ScrollToTop from "./components/helper/scroll-to-top";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Dev's Portfolio",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Dev Gupta. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/p.png" /> {/* Adjust the path if needed */}
+        {/* Add other head tags here if needed */}
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
