184439c6c4f9e448adbd4afcc5abc7cabec241d3
Devgupta1432 <100144102+Devgupta1432@users.noreply.github.com>
2024-10-17 21:40:08 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -8,7 +8,7 @@ export const personalData = {
   address: 'Huda sector-2 Palwal, Haryana - 121102 ',
   github: 'https://github.com/Devgupta1432',
   facebook: '',
-  linkedIn: 'linkedin.com/in/dev-gupta-0b4b461b5',
+  linkedIn: 'https://linkedin.com/in/dev-gupta-0b4b461b5',
   twitter: '',
   stackOverflow: '',
   leetcode: "",
