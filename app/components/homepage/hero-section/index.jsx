5438408da133da1611ecd9ff5ef0c3ef126cd418
Devgupta1432 <100144102+Devgupta1432@users.noreply.github.com>
2024-11-04 01:37:15 +0530

    Update index.jsx

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -88,7 +88,7 @@ function HeroSection() {
             <code className="font-mono text-xs md:text-sm lg:text-base">
               <div className="blink">
                 <span className="mr-2 text-pink-500">const</span>
-                <span className="mr-2 text-white">coder</span>
+                <span className="mr-2 text-white">candidate</span>
                 <span className="mr-2 text-pink-500">=</span>
                 <span className="text-gray-400">{'{'}</span>
               </div>
@@ -117,6 +117,10 @@ function HeroSection() {
                 <span className="text-amber-300">Java</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Figma</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Canva</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Adobe Photoshop</span>
                 <span className="text-gray-400">{"'],"}</span>
               </div>
               <div>
