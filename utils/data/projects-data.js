4227f85cb89d09e75475b9d3f17625fc766a1cea
Devgupta1432 <100144102+Devgupta1432@users.noreply.github.com>
2024-11-04 01:39:51 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -2,6 +2,10 @@ import ayla from '/public/image/ayla.jpg';
 import crefin from '/public/image/crefin.jpg';
 import realEstate from '/public/image/real-estate.jpg';
 import travel from '/public/image/travel.jpg';
+import home from '/public/image/home.jpg';
+import cosmetic from '/public/image/cosmetic.jpg';
+import stock from '/public/image/stock.jpg';
+
 
 export const projectsData = [
     {
@@ -33,6 +37,43 @@ export const projectsData = [
         role: 'Full Stack Developer',
         demo: '',
         image: realEstate,
+        
+    },
+   
+    {
+        id: 4,
+        name: 'Home Connect UI',
+        description: 'I have designed a UI/UX prototype for the Home Connect app, which seamlessly connects smart devices to your mobile phone. This app enhances user experience by allowing users to control and monitor various smart home devices from a single, intuitive interface. The design focuses on ease of navigation, visually appealing layouts, and clear functionality, ensuring that users can easily manage their smart home environment with just a few taps.',
+        tools: ['Figma'],
+        code: '',
+        role: 'Ui/Ux Developer',
+        demo: '',
+        behance: "https://www.behance.net/gallery/207329067/HomeConnect-Smart-Home-App",
+        image: home,
+    },
+   
+    {
+        id: 5,
+        name: 'Stock Market UI',
+        description: 'I have designed the user interface for a stock market app that provides users with real-time data and analytics for their investments. The UI focuses on delivering a clean and intuitive experience, enabling users to easily track stock performance, access market trends, and manage their investment portfolios. The design incorporates visually engaging charts and dashboards, ensuring that critical information is readily accessible and user-friendly.',
+        tools: ['Figma'],
+        code: '',
+        role: 'Ui/Ux Developer',
+        demo: '',
+        behance: "https://www.behance.net/gallery/204725029/Stock-Market-app-version-UI",
+        image: stock,
+    },
+   
+    {
+        id: 6,
+        name: 'Cosmetic Website',
+        description: 'I have designed the user interface for a cosmetic website that features over 30 web pages. The UI is crafted to provide an engaging and seamless shopping experience, showcasing a wide range of cosmetic products. Each page is thoughtfully designed to enhance usability and aesthetics, incorporating visually appealing layouts, intuitive navigation, and responsive design. The goal is to create a visually stunning platform that effectively highlights products while ensuring a smooth user journey.',
+        tools: ['Figma'],
+        code: '',
+        role: 'Ui/Ux Developer',
+        demo: '',
+        behance: "https://www.behance.net/gallery/204538079/Cosmetic-Website-UI",
+        image: cosmetic,
     },
    
 ];
